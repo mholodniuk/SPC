a795ab2fde361b6d09c38d5e2cb27a6860bf2bb3
maciejholodniuk <maciekholo@gmail.com>
2022-11-23 15:21:34 +0100

    update lab1 do sprawka

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -37,15 +37,24 @@ set(gcf,'color','w');
 set(0, 'DefaultLineLineWidth', 2);
 
 
-A = [4, 4, 2, 0, -1];
-B = [5, -5, 4, 1, 0];
+A = [4, -3, 2, 0, -1];
+B = [5, 14, 4, 1, 0];
+
+Legend = ["$a = 4, \quad b = 5$", "$a = -3, \quad b = 14$", ...
+    "$a = 2, \quad b = 4$", "$a = 0, \quad b = 1$", "$a = -1, \quad b = 0$"];
+Title = ["$z_{1} = -2+i, \quad z_{2} = -2-i$", "$z_{1} = 1.5+\frac{\sqrt{47}}{2}i, \quad z_{2} = 1.5-\frac{\sqrt{47}}{2}i$", ...
+    "$z_{1} = -1+1.75i, \quad z_{2} = -1-1.75i$", "$z_{1} = i, \quad z_{2} = -i$",  "$z_{1} = 0, \quad z_{2} = 1$"];
 
 for i = 1:length(B)
     a = A(i);
     b = B(i);
-    sim('zad2.slx', 100);
+    sim('zad2.slx', 20);
     
     subplot(5, 1, i);
     plot(ans.skok);
     hold on; grid on;
+    xlabel('');
+    ylabel('');
+    legend(Legend(i), 'Location', 'east', 'Interpreter', 'latex');
+    title(Title(i), 'Interpreter', 'latex', 'FontSize', 14);
 end
